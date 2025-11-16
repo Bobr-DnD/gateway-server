ce2c4300546eb0411032e5632961c608d9e75c8b
cyberpOOP <vlad0021135@gmail.com>
2025-11-16 20:15:43 +0200

    refactor webSocket proxy

--- utils/proxy.js
+++ utils/proxy.js
@@ -1,5 +1,5 @@
-// proxy.js
 import fastifyHttpProxy from '@fastify/http-proxy'
+import fastifyReplyFrom from '@fastify/reply-from'
 
 export default async function (fastify, opts) {
   fastify.register(fastifyHttpProxy, {
@@ -11,10 +11,11 @@ export default async function (fastify, opts) {
   })
 
   fastify.register(fastifyHttpProxy, {
-    upstream: `http://localhost:${process.env.SESSION_PORT}`,
-    prefix: '/session',
-    rewritePrefix: '/',
+    upstream: `http://localhost:${process.env.WS_PORT}`,
+    prefix: "/socket.io",
+    websocket: true, 
+    rewritePrefix: "/socket.io",
     http2: false,
     logLevel: 'silent'
-  })
+  });
 }
